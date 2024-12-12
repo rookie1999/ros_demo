d812840c8fe6e8fda5b11ab505f334be381a7e8b
benson.zhan <1456683521@qq.com>
2024-12-12 10:49:55 +0800

    Use gazebo to simulate motion_control, laser, camera, depth camera

---
+++ navigation/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/benson/projects/rosdemo/navigation/src
+# Build directory: /home/benson/projects/rosdemo/navigation/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ navigation/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/benson/projects/rosdemo/navigation/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/usr/local/cuda-12.0/lib64'
+export PWD='/home/benson/projects/rosdemo/navigation/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/benson/projects/rosdemo/navigation/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/benson/projects/rosdemo/navigation/src:$ROS_PACKAGE_PATH"
